8cbd39ed7292983f459af5d3a8edbfd2ad4787a5
T-Mo <44778821+tmcsorley@users.noreply.github.com>
2026-09-09 19:13:04 -0400

    Uploaded files

---
+++ Elden Ring Flowchart/Elden Ring Node Data.js
@@ -0,0 +1,251 @@
+// --- Javascript Node Data --- //
+
+export const nodeData = [
+                {
+                    id: "1",
+                    title: "Queen Marika the Eternal",
+                    image: "./img/Queen Marika.png",
+                    desc: "The deceased ruler of the Lands Between. Born a shaman of the spiritual Numen people, she became an Empyrean -- one deemed by Mother Metyr as a candidate for divine ascension. As such, she was granted her own Two Fingers for guidance, and a loyal beast to serve as her shadow. After conquering her opposition, she became a god, using her own golden hair to establish the Erdtree in the name of the Greater Will. She married Hoarah Loux, bestowing him his beast regent and the Lordly name Godfrey. After slaughtering the Fire Giants, her Golden Order became the dominant culture. At least until she shattered the Elden Ring...",
+                    x: "0",
+                    y: "0"
+                },
+                {
+                    id: "2",
+                    title: "Hoarah Loux",
+                    image: "./img/Godfrey and Hoarah Loux.jpg",
+                    desc: "",
+                    x: "0",
+                    y: "-2"
+                },
+                {
+                    id: '3',
+                    title: "Godwyn the Golden",
+                    image: "./img/Godwyn.jpg",
+                    desc: "",
+                    x: "-1",
+                    y: "-1"
+                },
+                {
+                    id: "4",
+                    title: "Radagon of the Golden Order",
+                    image: "./img/Radagon.jpg",
+                    desc: "The fervent red-haired Radagon descended from the Fire Giants, but spurned his origins. In the name of Marika's Golden Order, he took to battle, becoming a renowned champion. Arguably the most enigmatic figure at play, his history is obscure and plagued by perplexing events. During the Second Liurnian War, he desisted from his conquest to become the husband of Lady Rennala. After raising children together, Radagon returned to the royal capital, leaving Rennala with a unique blessing of the Erdtree. He became Queen Marika's second husband, and even became one with her corporeal body through unknown means. When she shattered the Elden Ring, they both crumbled and faded. Radagon, though, refused to fade away -- until he was finally slain by a Tarnished.",
+                    x: "0",
+                    y: "2"
+                },
+                {
+                    id: "5",
+                    title: "Rennala, Queen of the Full Moon",
+                    image: "./img/Rennala.jpg",
+                    desc: "A forlorn sorceress, long since lost to hysteria after being abandoned by her beloved spouse, Lord Radagon.",
+                    x: "0",
+                    y: "4"
+                },
+                {
+                    id: "6",
+                    title: "Radahn the Red-Maned Lion",
+                    image: "./img/The Lion General.jpg",
+                    desc: "Better known as Starscourge Radahn, this ferocious General was born a son of Radagon and Rennala. Thus, he was an inheritor of the Carian royal name, and displays a natural inclination for gravity magic. When his father left Rennala and married Queen Marika, Radahn and his siblings became demigods, each entitled to a seat before the Elden Throne. He's most famous for halting the stars in the Starscourge Conflict, and repelling Malenia's armies in the Battle of Aeonia. He was driven mad by her scarlet rot, becoming a feral beast until he was put to the sword by a Tarnished.",
+                    x: "-1",
+                    y: "3"
+                },
+                {
+                    id: "7",
+                    title: "Miquella the Kind",
+                    image: "./img/Miquella the God.jpg",
+                    desc: "",
+                    x: "-1",
+                    y: "1"
+                },
+                {
+                    id: "8",
+                    title: "Malenia the Severed",
+                    image: "./img/Malenia the Severed.jpg",
+                    desc: "Born of the unified Radagon and Marika, Malenia and her twin brother Miquella were cursed from birth. Malenia carried the essence of scarlet rot, leaving her flesh in a constant state of putrescence and decay. When her caring brother entered his cocoon, it became Malenia's sworn duty to slay the Lion General, allowing his soul to be ressurected by Miquella. Her chaotic battle with Radahn, the Battle of Aeonia, led to the decimation of Caelid. In allowing her scarlet rot to bloom, she flooded the land and sent the General into an undying frenzy. She fell into a comatose state and was carried to the Haligtree, where she slept until a Tarnished awoke her.",
+                    x: "-2",
+                    y: "1"
+                },
+                {
+                    id: "9",
+                    title: "Ranni the Witch",
+                    image: "./img/Ranni Corpse.jpg",
+                    desc: "",
+                    x: "-2",
+                    y: "3"
+                },
+                {
+                    id: "10",
+                    title: "Praetor Rykard",
+                    image: "./img/Rykard Expanded.jpg",
+                    desc: "",
+                    x: "-3",
+                    y: "3"
+                },
+                {
+                    id: "11",
+                    title: "Messmer",
+                    image: "./img/Messmer.jpg",
+                    desc: "",
+                    x: "-3",
+                    y: "1"
+                },
+                {
+                    id: "12",
+                    title: "Melina",
+                    image: "./img/Melina Burning.jpg",
+                    desc: "",
+                    x: "-4",
+                    y: "1"
+                },
+                {
+                    id: "13",
+                    title: "Mohg, Lord of Blood",
+                    image: "./img/Mohg Lord of Blood.jpg",
+                    desc: "",
+
+                    x: "-2",
+                    y: "-1"
+                },
+                {
+                    id: "14",
+                    title: "Morgott, Veiled Monarch",
+                    image: "./img/Morgott.jpg",
+                    desc: "",
+                    x: "-3",
+                    y: "-1"
+                },
+                {
+                    id: "15",
+                    title: "Godefroy the Grafted",
+                    image: "",
+                    desc: "",
+                    x: "-1",
+                    y: "-4"
+                },
+                {
+                    id: "16",
+                    title: "Godrick the Grafted",
+                    image: "./img/Godrick.jpg",
+                    desc: "",
+                    x: "-2",
+                    y: "-4"
+                },
+                {
+                    id: "17",
+                    title: "The Elden Beast",
+                    image: "./img/Elden Beast.jpg",
+                    desc: "Child of the Greater Will, sent in the age of the Primordial Crucible to establish the concept of Order. It is the essence of the Elden Ring itself, living within the monarch vessel known as Marika and Radagon.",
+                    x: "3",
+                    y: "4"
+                },
+                {
+                    id: "18",
+                    title: "Crucible Knights",
+                    image: "",
+                    desc: "",
+                    x: "1",
+                    y: "-3"
+                },
+                {
+                    id: "19",
+                    title: "Stormhawks",
+                    image: "",
+                    desc: "A faction of wild-spirited birds from the age before the Erdtree. Seemingly descended from the mountaintops of the giants, they became the initial inhabitants of Stormveil, where storms are constant. When Lord Godfrey slew the head of their flock, the storms were weakened, and many of the Stormhawks died from purposelessness. Those who survived were largely grafted into Warhawks after the construction of Stormveil Castle, specifically once Lord Godfrey was banished and his offspring usurped the throne.",
+                    x: "-1",
+                    y: "-4"
+                },
+                {
+                    id: "20",
+                    title: "Fire Giants",
+                    image: "",
+                    desc: "",
+                    x: "1",
+                    y: "3"
+                },
+                {
+                    id: "21",
+                    title: "Astrologers",
+                    image: "",
+                    desc: "",
+                    x: "1",
+                    y: "5"
+                },
+                {
+                    id: "22",
+                    title: "Tarnished",
+                    image: "",
+                    desc: "A term given to those who have lost the grace of gold. Particularly used to describe those following the guidance of grace, who aspire to become the Elden Lord. Lord Godfrey became the first Tarnished after his long march, when his purpose was fulfilled and he lost the grace of gold. Marika stripped this grace from those who had received it, and banished them all from The Lands Between. She claimed that a time would come when their grace would be restored, and they would be guided back to the Erdtree. After banishment, the Tarnished warriors adopted a variety of lives, and their descendants have since inherited their Tarnished nature. When Marika shattered the Elden Ring, the Two Fingers called out to the Tarnished by revealing to them the guidance of grace. Thus, those who would heed the call become true 'Tarnished,' and leave their newfound homes to return to the Lands Between. To seek the throne, and become the Elden Lord.",
+                    x: "0",
+                    y: "-8"
+                },
+
+
+
+
+
+                {
+                    id: "100",
+                    title: "Stormveil",
+                    image: "",
+                    desc: "",
+                    x: "-1",
+                    y: "-5"
+                },
+                {
+                    id: "101",
+                    title: "The Golden Order",
+                    image: "./img/Golden Order.jpg",
+                    desc: "A religious government structure established by Queek Marika. This Order was the subject of many champions' favor, as the Erdtree would grant blessings of gold upon any who served it dutifully.",
+                    x: "3",
+                    y: "1"
+                },
+                {
+                    id: "102",
+                    title: "The Elden Ring",
+                    image: "./img/Elden Ring.jpg",
+                    desc: "",
+                    x: "1",
+                    y: "1"
+                },
+                {
+                    id: "103",
+                    title: "The Erdtree",
+                    image: "",
+                    desc: "",
+                    x: "2",
+                    y: "1"
+                },
+                {
+                    id: "104",
+                    title: "The Crucible of Life",
+                    image: "",
+                    desc: "The primordial form of the Erdtree, it is also known as 'The Primordial Crucible' or simply 'The Crucible.' More an asbtract idea than a physical object. It was a culmination of vital life energies which, if Hyetta is to be believed, must have developed from the Fracture along with the earth. It birthed life in its first forms, along with the presence of souls, allowing life to have identity. When Queen Marika ascended to godhood as a vassal of the Greater Will, the Crucible and its essence were transformed into the Erdtree. Thus did the Greater Will impose order upon the lands.",
+                    x: "2",
+                    y: "-1"
+                },
+                {
+                    id: "105",
+                    title: "The Scadutree",
+                    image: "",
+                    desc: "The shadow of the Erdtree and its Golden Order. A manifestation of all that exists without.",
+                    x: "3",
+                    y: "0"
+                },
+                {
+                    id: "106",
+                    title: "Grace of Gold",
+                    image: "",
+                    desc: "This merely refers to the blessings of the Erdtree -- however, to the Tarnished, this grace manifests as a ray of light leading them down the path to becoming Elden Lord. The Tarnished have taken to calling this 'The Guidance of Grace.'",
+                    x: "1",
+                    y: "-5"
+                }
+            ];
+
+            export const nodeConnections = [
+                {
+                    id: "1",
+                    offspring: [3, 7, 13, 14],
+                    spouse: [2, 4]
+                }
+            ];
+            
